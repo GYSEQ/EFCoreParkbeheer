0cc3ced5d184d1ec39f0a617959f1cacc41a2115
Gyselinck, Quint <gyselinckq@delawareconsulting.com>
2023-12-25 12:32:42 +0100

    efcore model toegevoegd

---
+++ EFCoreParkbeheer.DL/ParkbeheerContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreParkbeheer.DL
+{
+    public class ParkbeheerContext : DbContext
+    {
+        private string connectionString;
+
+        public ParkbeheerContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
